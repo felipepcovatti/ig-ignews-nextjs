79324921936b6e3783a4500e3b971dcd7eb602f6
Felipe P Covatti <felipepcovatti@gmail.com>
2021-03-27 11:11:12 -0300

    Define basic layout and content of the home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head"
 
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
@@ -8,6 +9,17 @@ export default function Home() {
         <title>Home | ig.news</title>
       </Head>
 
+      <main className={styles.contentContainaer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world</h1>
+          <p>
+            Get accesss to all the publications <br />
+            <span>for $9.90 / month</span>
+          </p>
+        </section>
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
